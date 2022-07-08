15a8042a1001cd6c4416d690a9c99653259e0207
DongNH <hoaidongit5@gmail.com>
2022-07-08 11:55:49 +0700

    Delete file not use

--- ios/Pods/Target Support Files/Pods-DemoCamera-DemoCameraTests/Pods-DemoCamera-DemoCameraTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DemoCamera_DemoCameraTests : NSObject
-@end
-@implementation PodsDummy_Pods_DemoCamera_DemoCameraTests
-@end
--- ios/Pods/Target Support Files/Pods-DemoCamera/Pods-DemoCamera-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DemoCamera : NSObject
-@end
-@implementation PodsDummy_Pods_DemoCamera
-@end
